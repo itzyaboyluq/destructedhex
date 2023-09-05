e3374b0a010e16c5a11bad76baace42a3b0cbfeb
itzyaboyluq <143871213+itzyaboyluq@users.noreply.github.com>
2023-09-05 07:14:35 -0300

    Update dhexv1.4.lua

--- dhexv1.4.lua
+++ dhexv1.4.lua
@@ -136,7 +136,7 @@ local ranims = Instance.new("TextButton")
 local ragdoll = Instance.new("TextButton")
 local shutdown = Instance.new("TextButton")
 local punish = Instance.new("TextButton")
-local rwheel = Instance.new("TextButton")
+local rchassis = Instance.new("TextButton")
 
 --Properties:
 
@@ -460,15 +460,15 @@ punish.Text = "Punish"
 punish.TextColor3 = Color3.fromRGB(255, 255, 255)
 punish.TextSize = 14.000
 
-rwheel.Name = "rwheel"
-rwheel.Parent = scripts
-rwheel.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
-rwheel.BorderSizePixel = 0
-rwheel.Size = UDim2.new(0, 200, 0, 50)
-rwheel.Font = Enum.Font.Roboto
-rwheel.Text = "Rwheel"
-rwheel.TextColor3 = Color3.fromRGB(255, 255, 255)
-rwheel.TextSize = 14.000
+rchassis.Name = "rchassis"
+rchassis.Parent = scripts
+rchassis.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+rchassis.BorderSizePixel = 0
+rchassis.Size = UDim2.new(0, 200, 0, 50)
+rchassis.Font = Enum.Font.Roboto
+rchassis.Text = "Rchassis"
+rchassis.TextColor3 = Color3.fromRGB(255, 255, 255)
+rchassis.TextSize = 14.000
 
 local bannedPlayers = {}
 local serverlock = false
@@ -770,9 +770,9 @@ end)
 end
 end)
 
-rwheel.MouseButton1Click:Connect(function()
+rchassis.MouseButton1Click:Connect(function()
 for i, v in pairs(game:GetService("Workspace"):GetDescendants()) do
-if string.match(v.Name, "Wheel") then
+if string.match(v.Name, "Chassis") then
 work(v)
 end
 end
