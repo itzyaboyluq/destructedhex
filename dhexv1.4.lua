86b4275d554dbbfc53e5c8ceb34caf7e63cec740
itzyaboyluq <143871213+itzyaboyluq@users.noreply.github.com>
2023-09-05 07:02:34 -0300

    Update dhexv1.4.lua

--- dhexv1.4.lua
+++ dhexv1.4.lua
@@ -772,7 +772,7 @@ end)
 
 unanchor.MouseButton1Click:Connect(function()
 for i, v in pairs(game:GetService("Workspace"):GetDescendants()) do
-if v:IsA("Motor6D") then
+if v:FindFirstChild("Rotate") then
 work(v)
 end
 end
